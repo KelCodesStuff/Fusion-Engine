1c383a840cc05eb1dede0bc4f306ab17cb6d27e2
KelCodesStuff <codesmithk@gmail.com>
2025-08-15 11:10:41 -0400

    Initial Commit

---
+++ Fusion-Engine/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Fusion-Engine
+//
+//  Created by Kelvin Reid on 8/15/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ Fusion-EngineTests/Fusion_EngineTests.swift
@@ -0,0 +1,17 @@
+//
+//  Fusion_EngineTests.swift
+//  Fusion-EngineTests
+//
+//  Created by Kelvin Reid on 8/15/25.
+//
+
+import Testing
+@testable import Fusion_Engine
+
+struct Fusion_EngineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
